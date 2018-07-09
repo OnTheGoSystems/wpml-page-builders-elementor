d1a0a382c24c70ae4dedf0cd80c22b1ae03aa8d2
Bruce Pearson <bruce@icanlocalize.com>
2018-07-09 15:10:25 +0800

    Translated the id in the Elementor template widget - https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlcore-5433

--- src/class-wpml-elementor-translate-ids.php
+++ src/class-wpml-elementor-translate-ids.php
@@ -10,21 +10,21 @@ class WPML_Elementor_Translate_IDs implements IWPML_Action {
 	}
 
 	public function add_hooks() {
-		add_filter( 'elementor/theme/get_location_templates/template_id', array( $this, 'translate_template_id' ) );
+		add_filter( 'elementor/theme/get_location_templates/template_id', array( $this, 'translate_theme_location_template_id' ) );
 		add_filter( 'elementor/documents/get/post_id', array(
 			$this,
-			'translate_template_id_for_WP_widget_and_shortcode'
+			'translate_template_id'
 		) );
 		add_filter( 'elementor/frontend/builder_content_data', array( $this, 'translate_global_widget_ids' ), 10, 2 );
 	}
 
-	public function translate_template_id( $template_id ) {
-		return apply_filters( 'wpml_object_id', $template_id, get_post_type( $template_id ), true );
+	public function translate_theme_location_template_id( $template_id ) {
+		return $this->translate_id( $template_id );
 	}
 
-	public function translate_template_id_for_WP_widget_and_shortcode( $template_id ) {
-		if ( $this->is_WP_widget_call() || $this->is_shortcode_call() ) {
-			$template_id = $this->translate_template_id( $template_id );
+	public function translate_template_id( $template_id ) {
+		if ( $this->is_WP_widget_call() || $this->is_shortcode_call() || $this->is_template_widget_call() ) {
+			$template_id = $this->translate_id( $template_id );
 		}
 
 		return $template_id;
@@ -42,14 +42,24 @@ class WPML_Elementor_Translate_IDs implements IWPML_Action {
 			'shortcode' );
 	}
 
+	private function is_template_widget_call() {
+		return $this->debug_backtrace->is_class_function_in_call_stack(
+			'ElementorPro\Modules\Library\Widgets\Template',
+			'render' );
+	}
+
 	public function translate_global_widget_ids( $data_array, $post_id ) {
 		foreach ( $data_array as &$data ) {
 			if ( isset( $data['elType'] ) && 'widget' === $data['elType'] && 'global' === $data['widgetType'] ) {
-				$data['templateID'] = $this->translate_template_id( $data['templateID'] );
+				$data['templateID'] = $this->translate_id( $data['templateID'] );
 			}
 			$data['elements'] = $this->translate_global_widget_ids( $data['elements'], $post_id );
 		}
 
 		return $data_array;
+	}
+
+	private function translate_id( $template_id ) {
+		return apply_filters( 'wpml_object_id', $template_id, get_post_type( $template_id ), true );
 	}
 }
--- tests/phpunit/tests/test-wpml-elementor-translate-ids.php
+++ tests/phpunit/tests/test-wpml-elementor-translate-ids.php
@@ -11,11 +11,11 @@ class Test_WPML_Elementor_Translate_IDs extends OTGS_TestCase {
 
 		$this->expectFilterAdded( 'elementor/theme/get_location_templates/template_id', array(
 			$subject,
-			'translate_template_id'
+			'translate_theme_location_template_id'
 		), 10 );
 		$this->expectFilterAdded( 'elementor/documents/get/post_id', array(
 			$subject,
-			'translate_template_id_for_WP_widget_and_shortcode'
+			'translate_template_id'
 		), 10 );
 		$this->expectFilterAdded( 'elementor/frontend/builder_content_data', array(
 			$subject,
@@ -48,7 +48,7 @@ class Test_WPML_Elementor_Translate_IDs extends OTGS_TestCase {
 
 		$subject = new WPML_Elementor_Translate_IDs( \Mockery::mock( 'WPML_Debug_BackTrace' ) );
 
-		$this->assertEquals( $translated_id, $subject->translate_template_id( $template_id ) );
+		$this->assertEquals( $translated_id, $subject->translate_theme_location_template_id( $template_id ) );
 
 	}
 
@@ -79,7 +79,7 @@ class Test_WPML_Elementor_Translate_IDs extends OTGS_TestCase {
 
 		$subject = new WPML_Elementor_Translate_IDs( $debug_backtrace );
 
-		$this->assertEquals( $translated_id, $subject->translate_template_id_for_WP_widget_and_shortcode( $template_id ) );
+		$this->assertEquals( $translated_id, $subject->translate_template_id( $template_id ) );
 
 	}
 
@@ -107,14 +107,50 @@ class Test_WPML_Elementor_Translate_IDs extends OTGS_TestCase {
 		$debug_backtrace->shouldReceive( 'is_class_function_in_call_stack' )
 		                ->with( 'ElementorPro\Modules\Library\WP_Widgets\Elementor_Library', 'widget' )
 		                ->andReturn( false );
-
 		$debug_backtrace->shouldReceive( 'is_class_function_in_call_stack' )
 		                ->with( 'ElementorPro\Modules\Library\Classes\Shortcode', 'shortcode' )
 		                ->andReturn( true );
 
 		$subject = new WPML_Elementor_Translate_IDs( $debug_backtrace );
 
-		$this->assertEquals( $translated_id, $subject->translate_template_id_for_WP_widget_and_shortcode( $template_id ) );
+		$this->assertEquals( $translated_id, $subject->translate_template_id( $template_id ) );
+
+	}
+
+	/**
+	 * @test
+	 */
+	public function it_should_translate_template_id_for_template_widget() {
+
+		$template_id   = 123;
+		$translated_id = 456;
+		$post_type     = 'anything';
+
+		\WP_Mock::userFunction( 'get_post_type',
+			array(
+				'args'   => $template_id,
+				'return' => $post_type
+			)
+		);
+
+		\WP_Mock::onFilter( 'wpml_object_id' )
+		        ->with( $template_id, $post_type, true )
+		        ->reply( $translated_id );
+
+		$debug_backtrace = \Mockery::mock( 'WPML_Debug_BackTrace' );
+		$debug_backtrace->shouldReceive( 'is_class_function_in_call_stack' )
+		                ->with( 'ElementorPro\Modules\Library\WP_Widgets\Elementor_Library', 'widget' )
+		                ->andReturn( false );
+		$debug_backtrace->shouldReceive( 'is_class_function_in_call_stack' )
+		                ->with( 'ElementorPro\Modules\Library\Classes\Shortcode', 'shortcode' )
+		                ->andReturn( false );
+		$debug_backtrace->shouldReceive( 'is_class_function_in_call_stack' )
+		                ->with( 'ElementorPro\Modules\Library\Widgets\Template', 'render' )
+		                ->andReturn( true );
+
+		$subject = new WPML_Elementor_Translate_IDs( $debug_backtrace );
+
+		$this->assertEquals( $translated_id, $subject->translate_template_id( $template_id ) );
 
 	}
 
@@ -144,8 +180,7 @@ class Test_WPML_Elementor_Translate_IDs extends OTGS_TestCase {
 
 		$subject = new WPML_Elementor_Translate_IDs( $debug_backtrace );
 
-		$this->assertEquals( $template_id, $subject->translate_template_id_for_WP_widget_and_shortcode( $template_id ) );
-
+		$this->assertEquals( $template_id, $subject->translate_template_id( $template_id ) );
 	}
 
 }
