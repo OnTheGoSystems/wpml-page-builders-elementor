6ec0bb8f5b9e31b20312b42c1a7e084ead46627f
Bruce Pearson <bruce@icanlocalize.com>
2018-07-10 16:15:21 +0800

    [FIXUP]

--- src/class-wpml-elementor-urls.php
+++ src/class-wpml-elementor-urls.php
@@ -2,15 +2,12 @@
 
 class WPML_Elementor_URLs implements IWPML_Action {
 
-	/** @var SitePress  */
-	private $sitepress;
-
 	public function __construct(
 		WPML_Translation_Element_Factory $element_factory,
 		WPML_Language_Domains $domains = null
 	) {
 		$this->element_factory = $element_factory;
-		$this->domains = $domains;
+		$this->domains         = $domains;
 	}
 
 	public function add_hooks() {
@@ -22,12 +19,13 @@ class WPML_Elementor_URLs implements IWPML_Action {
 	public function adjust_edit_with_elementor_url( $url, $elementor_document ) {
 		$post = $elementor_document->get_main_post();
 
-		$post_element = $this->element_factory->create( $post->ID, WPML_Translation_Element_Factory::ELEMENT_TYPE_POST );
+		$post_element  = $this->element_factory->create( $post->ID, WPML_Translation_Element_Factory::ELEMENT_TYPE_POST );
 		$post_language = $post_element->get_language_code();
 
 		if ( $post_language ) {
-			$url_parts = wpml_parse_url( $url );
-			$url       = $url_parts['scheme'] . '://' . $this->domains->get( $post_language ) . $url_parts['path'] . '?' . $url_parts['query'];
+			$url_parts         = wpml_parse_url( $url );
+			$url_parts['host'] = $this->domains->get( $post_language );
+			$url               = http_build_url( $url_parts );
 		}
 
 		return $url;
