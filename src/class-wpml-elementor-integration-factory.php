919b1a998cc0804e7eaa6584630a4ca90dec7bc8
Adriano Ferreira <adriano.f@onthegosystems.com>
2018-08-08 14:23:44 -0300

    Handling page without translatable elements
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlpb-146

--- src/class-wpml-elementor-integration-factory.php
+++ src/class-wpml-elementor-integration-factory.php
@@ -16,6 +16,7 @@ class WPML_Elementor_Integration_Factory {
 				'WPML_Elementor_URLs_Factory',
 				'WPML_Elementor_Media_Translation_Factory',
 				'WPML_Elementor_Adjust_Global_Widget_ID_Factory',
+				'WPML_PB_Elementor_Handle_Custom_Fields_Factory',
 			)
 		);
 
--- tests/phpunit/tests/test-wpml-elementor-integration-factory.php
+++ tests/phpunit/tests/test-wpml-elementor-integration-factory.php
@@ -21,10 +21,11 @@ class Test_WPML_Elementor_Integration_Factory extends OTGS_TestCase {
 		$action_filter_loader->shouldReceive( 'load' )
 		                     ->once()
 		                     ->with( array(
-		                     	'WPML_Elementor_Translate_IDs_Factory',
-		                     	'WPML_Elementor_URLs_Factory',
+			                     'WPML_Elementor_Translate_IDs_Factory',
+			                     'WPML_Elementor_URLs_Factory',
 			                     'WPML_Elementor_Media_Translation_Factory',
 			                     'WPML_Elementor_Adjust_Global_Widget_ID_Factory',
+			                     'WPML_PB_Elementor_Handle_Custom_Fields_Factory',
 		                     ) );
 
 		$string_registration = \Mockery::mock( 'overload:WPML_PB_String_Registration' );
