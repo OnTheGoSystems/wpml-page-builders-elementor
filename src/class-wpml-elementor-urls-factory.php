5677e4cb4b7ba2952e3ef5c4e2d7843a5c4fd9d5
Bruce Pearson <bruce@icanlocalize.com>
2018-08-20 11:27:44 +0800

    Fix problem with loading elementor global widgets - https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlcore-5606

--- src/class-wpml-elementor-urls-factory.php
+++ src/class-wpml-elementor-urls-factory.php
@@ -1,17 +1,14 @@
 <?php
 
-class WPML_Elementor_URLs_Factory implements IWPML_Backend_Action_Loader {
+class WPML_Elementor_URLs_Factory implements IWPML_Backend_Action_Loader, IWPML_Frontend_Action_Loader {
 
 	public function create() {
-		global $sitepress, $wpdb;
+		global $sitepress, $wpml_url_converter;
 
-		$domains = null;
-		if ( WPML_LANGUAGE_NEGOTIATION_TYPE_DOMAIN === (int) $sitepress->get_setting( 'language_negotiation_type' ) ) {
-			$domains = new WPML_Language_Domains( $sitepress, new WPML_URL_Converter_Url_Helper( $wpdb ) );
-		}
-
-		$element_factory = new WPML_Translation_Element_Factory( $sitepress );
-
-		return new WPML_Elementor_URLs( $element_factory, $domains, $sitepress );
+		return new WPML_Elementor_URLs(
+			new WPML_Translation_Element_Factory( $sitepress ),
+			$wpml_url_converter->get_strategy(),
+			$sitepress
+		);
 	}
 }
