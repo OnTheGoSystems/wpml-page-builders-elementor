30da200661bf7f6b18fbdbc97b2202d603153308
Adriano Ferreira <adriano.f@onthegosystems.com>
2019-02-04 09:10:27 -0200

    Fixed PHP warning when dealing with _elementor_css field
    The problem happens because there is another indexed level inside of the field value (apparently changed recently on Elementor, not sure though).
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlcore-6319

--- src/class-wpml-elementor-data-settings.php
+++ src/class-wpml-elementor-data-settings.php
@@ -35,7 +35,13 @@ class WPML_Elementor_Data_Settings implements IWPML_Page_Builders_Data_Settings 
 	 */
 	public function mark_css_field_as_empty( $value, $translated_post_id, $original_post_id, $meta_key ) {
 		if ( '_elementor_css' === $meta_key ) {
-			$value['status'] = '';
+			if ( ! isset( $value['status'] ) ) {
+				$value           = current( $value );
+				$value['status'] = '';
+				$value           = array( $value );
+			} else {
+				$value['status'] = '';
+			}
 		}
 
 		return $value;
