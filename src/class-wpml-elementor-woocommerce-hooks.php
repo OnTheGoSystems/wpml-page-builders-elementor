ae93a3a399f9627482300ad9c69053d58db285fa
Andrea Sciamanna <andrea.s@onthegosystems.com>
2019-02-25 16:35:39 +0100

    Notices and warnings could appear in the debug.log when using WPML + Elementor
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlcore-6400

--- src/class-wpml-elementor-woocommerce-hooks.php
+++ src/class-wpml-elementor-woocommerce-hooks.php
@@ -12,9 +12,9 @@ class WPML_Elementor_WooCommerce_Hooks {
 	 * @return WP_Query
 	 */
 	public function do_not_suppress_filters_on_product_widget( WP_Query $query ) {
-		if ( 'product' === $query->query_vars['post_type']
-		     && isset( $_POST['action'] )
-		     && 'elementor_ajax' === $_POST['action']
+		if (
+			array_key_exists( 'post_type', $query->query_vars ) && 'product' === $query->query_vars['post_type']
+			&& isset( $_POST['action'] ) && 'elementor_ajax' === $_POST['action']
 		) {
 			$query->query_vars['suppress_filters'] = false;
 		}
