b81f0e1b891b06eb9d6f6dad522ffe1b15dd9f76
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2020-03-26 16:25:16 -0300

    Fix Media Carousel link not auto-converted
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlcore-7149

---
+++ tests/phpunit/tests/modules/TestMediaCarousel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WPML\PB\Elementor\Modules;
+
+/**
+ * @group modules
+ * @group wpmlcore-7149
+ */
+class TestMediaCarousel extends \OTGS_TestCase {
+
+	/**
+	 * @test
+	 */
+	public function it_get_fields() {
+		$subject = new MediaCarousel();
+		$this->assertEquals( [ 'image_link_to' => [ 'url' ] ], $subject->get_fields() );
+	}
+
+	/**
+	 * @test
+	 */
+	public function it_get_items_field() {
+		$subject = new MediaCarousel();
+		$this->assertEquals( 'slides', $subject->get_items_field() );
+	}
+}
