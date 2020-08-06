65e2e0c4982ac661c0e28f5661feee88127f2eae
Pierre SYLVESTRE <pierre.s@onthegosystems.com>
2020-08-06 17:48:13 -0300

    Prevent mix of string packages if a post was initially edited with Gutenberg and then with Elementor
    When Elementor is saving its data, we'll try to:
    - detect some Gutenberg block meta data in all editor widgets and remove it
    - if Elementor's data has changed, update the data
    - remove the Gutenberg package
    
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlcore-7444

---
+++ tests/phpunit/tests/Hooks/TestGutenbergCleanup.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace WPML\PB\Elementor\Hooks;
+
+use tad\FunctionMocker\FunctionMocker;
+use WPML\FP\Fns;
+use WPML\LIB\WP\PostMock;
+use WPML\LIB\WP\Post;
+use WPML_Elementor_Data_Settings;
+
+/**
+ * @group hooks
+ * @group gutenberg-cleanup
+ */
+class TestGutenbergCleanup extends \OTGS_TestCase {
+
+	use PostMock;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->setUpPostMock();
+
+		\WP_Mock::passthruFunction( 'wp_slash' );
+		\WP_Mock::userFunction( 'wp_json_encode', [
+			'return' => function( $value ) {
+				return json_encode( $value );
+			},
+		] );
+	}
+
+	/**
+	 * @test
+	 */
+	public function itShouldAddHooks() {
+		$subject = new GutenbergCleanup();
+
+		\WP_Mock::expectFilterAdded(
+			'update_post_metadata',
+			Fns::withoutRecursion( Fns::identity(), [ $subject, 'removeGutenbergFootprint' ] ),
+			10,
+			4
+		);
+
+		$subject->add_hooks();
+	}
+
+	/**
+	 * @test
+	 */
+	public function itShouldNOTRemoveGbFootprintIfNotElementorDataMeta() {
+		$check     = 'some boolean';
+		$postId    = 123;
+		$metaKey   = 'some key';
+		$metaValue = 'some value';
+
+		$subject = new GutenbergCleanup();
+
+		$this->assertSame(
+			$check,
+			$subject->removeGutenbergFootprint( $check, $postId, $metaKey, $metaValue )
+		);
+	}
+
+	/**
+	 * @test
+	 */
+	public function itShouldNOTRemoveGbFootprintIfPostNOTEditedWithElementor() {
+		$check     = 'some boolean';
+		$postId    = 123;
+		$metaKey   = WPML_Elementor_Data_Settings::META_KEY_DATA;
+		$metaValue = 'some value';
+
+		Post::updateMeta( $postId, WPML_Elementor_Data_Settings::META_KEY_DATA, $metaValue );
+
+		$isEditedWithElementor = FunctionMocker::replace( WPML_Elementor_Data_Settings::class . '::is_edited_with_elementor', false );
+
+		$subject = new GutenbergCleanup();
+
+		$this->assertSame(
+			$check,
+			$subject->removeGutenbergFootprint( $check, $postId, $metaKey, $metaValue )
+		);
+
+		$isEditedWithElementor->wasCalledWithOnce( [ $postId ] );
+		$this->assertEquals( $metaValue, Post::getMetaSingle( $postId, WPML_Elementor_Data_Settings::META_KEY_DATA ) );
+	}
+
+	/**
+	 * @test
+	 */
+	public function itShouldNOTRemoveGbFootprintIfNOBlockIsFound() {
+		$check     = 'some boolean';
+		$postId    = 123;
+		$metaKey   = WPML_Elementor_Data_Settings::META_KEY_DATA;
+		$metaValue = self::getMetaValue( '<p><\/p>\n<p>Something sure!<\/p>\n<p><\/p>' );
+
+		Post::updateMeta( $postId, WPML_Elementor_Data_Settings::META_KEY_DATA, $metaValue );
+
+		$isEditedWithElementor = FunctionMocker::replace( WPML_Elementor_Data_Settings::class . '::is_edited_with_elementor', true );
+
+		\WP_Mock::userFunction( 'update_post_metadata' )->never();
+
+		$subject = new GutenbergCleanup();
+
+		$this->assertSame(
+			$check,
+			$subject->removeGutenbergFootprint( $check, $postId, $metaKey, $metaValue )
+		);
+
+		$isEditedWithElementor->wasCalledWithOnce( [ $postId ] );
+		$this->assertEquals( $metaValue, Post::getMetaSingle( $postId, WPML_Elementor_Data_Settings::META_KEY_DATA ) );
+	}
+
+	/**
+	 * @test
+	 */
+	public function itShouldRemoveGbFootprintIfOneBlockIsFound() {
+		$check        = 'some boolean';
+		$postId       = 123;
+		$metaKey      = WPML_Elementor_Data_Settings::META_KEY_DATA;
+		$metaValue    = self::getMetaValue( '<p><!-- wp:paragraph --><\/p><!-- /wp:paragraph -->\n<p>Something sure!<\/p>\n<p><\/p>' );
+		$newMetaValue = self::getMetaValue( '<p><\/p>\n<p>Something sure!<\/p>\n<p><\/p>' );
+
+		Post::updateMeta( $postId, WPML_Elementor_Data_Settings::META_KEY_DATA, $metaValue );
+
+		$elementorPackage = $this->getPackage( 'elementor', $postId );
+		$gutenbergPackage = $this->getPackage( 'gutenberg', $postId );
+		$unknownPackage   = $this->getPackage( 'unknown', $postId );
+
+		$isEditedWithElementor = FunctionMocker::replace( WPML_Elementor_Data_Settings::class . '::is_edited_with_elementor', true );
+
+		\WP_Mock::onFilter( 'wpml_st_get_post_string_packages' )
+			->with( [], $postId )
+			->reply( [ $elementorPackage, $gutenbergPackage, $unknownPackage ] );
+
+		$deleteAction = FunctionMocker::replace( 'do_action' );
+
+		$subject = new GutenbergCleanup();
+
+		$this->assertTrue(
+			$subject->removeGutenbergFootprint( $check, $postId, $metaKey, $metaValue )
+		);
+
+		$isEditedWithElementor->wasCalledWithOnce( [ $postId ] );
+		$this->assertEquals( $newMetaValue, Post::getMetaSingle( $postId, WPML_Elementor_Data_Settings::META_KEY_DATA ) );
+		$deleteAction->wasCalledWithOnce( [ 'wpml_delete_package', $gutenbergPackage->name, $gutenbergPackage->kind ] );
+	}
+
+	private static function getMetaValue( $editorContent ) {
+		return '[{"id":"dc45eec","elType":"section","settings":[],"elements":[{"id":"761adbdf","elType":"column","settings":{"_column_size":100},"elements":[{"id":"88004c2","elType":"widget","settings":{"title":"Add Your Heading Text Here"},"elements":[],"widgetType":"heading"},{"id":"888e41","elType":"widget","settings":{"editor":"' . $editorContent . '","__globals__":{"text_color":"globals\/colors?id=accent"}},"elements":[],"widgetType":"text-editor"}],"isInner":false}],"isInner":false}]';
+	}
+
+	private function getPackage( $kind, $name ) {
+		$package = $this->getMockBuilder( '\WPML_Package' )->getMock();
+		$package->kind_slug = strtolower( $kind );
+		$package->kind      = strtoupper( $kind );
+		$package->name      = $name;
+
+		return $package;
+	}
+}
